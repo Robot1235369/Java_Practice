647584d26b41bc63624157899890a363bee78f84
Robot1235369 <IsaacCA2018@gmail.com>
2021-09-15 15:50:24 +0000

    made changes

--- RPS.java
+++ RPS.java
@@ -1,5 +1,32 @@
+import java.util.*;
+
 class Main {
    public static void main(String args[]) {
-      System.out.println("Hello World");
+      Bool playAgain = true;
+      System.out.println("Welcome to Rock Paper Scissors!\n");
+
+      do {
+         Random random = new Random();
+         int upper = 3;
+         int rand = random.nextInt(upper);
+
+         Switch (rand) {
+            case 0:
+               char computer = "R"
+               default;
+            case 1:
+               char computer = "P"
+               default;
+            case 2:
+               default;
+         }
+
+         Scanner Choice = new Scanner(System.in);
+         System.out.print("R, P, or S: ");
+         char choice = Choice.next().charAt(0);
+
+         
+      } while (playAgain == true);
+
    }
 }
