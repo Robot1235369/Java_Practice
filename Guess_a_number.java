0312afaa0256cba405478ed00afa52afa0c4e008
Isaac <you@example.com>
2021-09-17 15:57:44 -0500

    finished Guess_a_number.java

--- Guess_a_number.java
+++ Guess_a_number.java
@@ -4,28 +4,28 @@ public class Guess_a_number {
     public static void main(String args[]) {
         int difficulty = 0;
         int guess;
-        int guesses;
         int win = 0;
         char playagain = 'y';
 
         System.out.println("Welcome to guess a number\n");
 
         do {
+            System.out.print("Enter a difficulty: ");
             Scanner scan = new Scanner(System.in);
             difficulty = scan.nextInt();
         } while (difficulty == 0);
 
         do {
-            guesses = 5;
             Random random = new Random();
             int rand = random.nextInt(difficulty++);
 
             if (rand == 0) {
                 continue;
             }
+            System.out.println(rand);
 
-            System.out.println("you have 5 guesses");
-            do {
+            System.out.println("\nyou have 5 guesses");
+            for (int guesses = 4; guesses != -1; guesses--) {
                 System.out.print("guess a number 1 - your chosen diffuculty: ");
                 Scanner scan = new Scanner(System.in);
                 guess = scan.nextInt();
@@ -33,7 +33,16 @@ public class Guess_a_number {
                     win = 1;
                     break;
                 }
-            } while (guess != 1);
+                if (guesses != 0) {
+                    System.out.print("You have ");
+                    System.out.print(guesses);
+                    if (guesses == 1) {
+                        System.out.println(" guess left");
+                    } else {
+                        System.out.println(" guesses left");
+                    }
+                }
+            }
 
             if (win == 1) {
                 System.out.println("You got it!");
@@ -43,7 +52,7 @@ public class Guess_a_number {
 
             do {
                 Scanner scan = new Scanner(System.in);
-                System.out.print("Do you want to play again? ");
+                System.out.print("Do you want to play again? (y/n): ");
                 playagain = scan.next().charAt(0);
             } while (playagain != 'y' && playagain != 'n');
 
